47b4ab7db97474bf8132f5b5fcab14649af6eef4
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2024-02-27 19:13:35 +0100

    Últimos detalles tres en raya

--- RamosReyesSáenzProyecto2Evaluación/src/Cliente_view.java
+++ RamosReyesSáenzProyecto2Evaluación/src/Cliente_view.java
@@ -1,5 +1,3 @@
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,17 +16,22 @@ import java.awt.event.ActionEvent;
 
 public class Cliente_view {
 
-	// Variable turno
+	// Variables para la gestión de la partida y la conexión
+	private static Cliente_view window;
 	public Integer turno;
 	private static DataInputStream dis;
-	private static DataOutputStream dos = null;
-
+	private static DataOutputStream dos;
+	private static Socket socketDelCliente;
 	private static String valorSimbolo = "";
 	private static String valorSimboloOponente = "";
 	private static String mensajeFinal = "";
-	private static Socket socketDelCliente;
+	private static String respuesta;
+	private static String ipServidor;
+	private static String nombreBotonMemoria;
+	//private static String log;
 
 	// Botones interfaz
+	private static JButton[] arraybotones = new JButton[9];
 	private static JButton btn_A1;
 	private static JButton btn_A2;
 	private static JButton btn_A3;
@@ -38,196 +41,187 @@ public class Cliente_view {
 	private static JButton btn_C1;
 	private static JButton btn_C2;
 	private static JButton btn_C3;
-
-	private JFrame frmTresEnRaya;
-	private static Cliente_view window;
-	private static String respuesta;
-	private static String ipServidor;
 	private static JLabel lb_usuario;
 	private static JLabel lb_simbolo;
-
-	private static String nombreBotonMemoria;
-	private static JButton[] arraybotones = new JButton[9];
-	private static String log;
+	private JFrame frmTresEnRaya;
 
 	public static void main(String[] args) {
-		
-				// 1. SE INICIA LA INTERFAZ
-				try {
-					window = new Cliente_view();
-					window.frmTresEnRaya.setVisible(false);
 
+		// Se inicia la interfaz
+		try {
+			window = new Cliente_view();
+			window.frmTresEnRaya.setVisible(false);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		ipServidor = JOptionPane.showInputDialog(null, "Introduce la IP del servidor.");
+
+		// Se intenta que el cliente se conecte con el servidor
+		InetAddress direcc = null;
+		if (!ipServidor.isEmpty()) {
+			try {
+				direcc = InetAddress.getByName(ipServidor);
+				int puerto = 40000;
+				try {
+					socketDelCliente = new Socket(direcc, puerto);
+					System.out.println("Cliente conectado");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+			} catch (UnknownHostException uhe) {
+				JOptionPane.showMessageDialog(null, "La IP introducida no es válida", "Error", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "No has introducido ninguna IP", "Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
 
+		// Se pide el nombre al jugador
+		respuesta = JOptionPane.showInputDialog(null, "¿Cuál es tu nombre?");
+		if (respuesta != null && !respuesta.isEmpty()) {
+			window.frmTresEnRaya.setVisible(true);
+		} else {
+			JOptionPane.showMessageDialog(null, "Cerrando la aplicación", "Cerrando", JOptionPane.INFORMATION_MESSAGE);
+			System.exit(0);
+		}
 
-				ipServidor = JOptionPane.showInputDialog(null, "Introduce la IP del servidor.");				
-				// 3. EL CLIENTE SE CONECTA CON EL SERVIDOR
-				InetAddress direcc = null;
-				if(!ipServidor.isEmpty()) {
+		// Se envía el nombre al servidor y se marca el nombre de los jugadores en sus
+		// respectivas interfaces
+		try {
+			if (socketDelCliente != null) {
+				dos = new DataOutputStream(socketDelCliente.getOutputStream());
+				dis = new DataInputStream(socketDelCliente.getInputStream());
+				dos.writeUTF(respuesta);
+				lb_usuario.setText(respuesta);
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "No se ha podido conectar con el servidor", "Error", JOptionPane.ERROR_MESSAGE);
+		}
 
-					try {
-						direcc = InetAddress.getByName(ipServidor); // la ip del servidor
-						int puerto = 40000;
-						try {
-							socketDelCliente = new Socket(direcc, puerto);
-							System.out.println("Cliente conectado");
+		while (true) {
+			// En este punto, el cliente se encuentra constantemente esperando un mensaje del servidor
+			// Dependiendo de cuál sea el valor del primer carácter de "input", tendrá lugar un efecto u otro
+			// El valor de "input" en muchos casos va a ser una concatenación de un número (un evento) y un mensajeFinal (un valor o mensaje)
+			
+			try {
+				String input = dis.readUTF();
 
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					} catch (UnknownHostException uhe) {
-						JOptionPane.showMessageDialog(null, "Ip introducida no válida", "Error",
-								JOptionPane.ERROR_MESSAGE);
-						System.exit(0);
+				// "1" - Se envía el número 1 junto al símbolo para gestionar la asignación de X e O ("1" + this.simbolo)
+				if (input.startsWith("1")) {
+					valorSimbolo = (input.charAt(1) + "");
+					if (valorSimbolo.equals("X")) {
+						valorSimboloOponente = "O";
+					} else {
+						valorSimboloOponente = "X";
+						bloquearBotones();
 					}
-				}else {
-					JOptionPane.showMessageDialog(null, "No has introducido ninguna ip", "Error",
-							JOptionPane.ERROR_MESSAGE);
-					System.exit(0);
-				}
+					lb_simbolo.setText(valorSimbolo);
 
-				// 2. APARECE LA INTERFAZ QUE PIDE EL NOMBRE
-				respuesta = JOptionPane.showInputDialog(null, "¿Cuál es tu nombre?");
+				// "2" - Se gestiona el evento de fin de partida, mostrando los avisos y el log de partidas
+				} else if (input.startsWith("2")) {
+					mensajeFinal = input.substring(1);
+					String mensajeResultado = mensajeFinal.split("\n")[0];
+					String logFinal = "";
+					String[] array = mensajeFinal.split("\n");
+					for (int i = 0; i < array.length; i++) {
+						if (i != 0) {
+							logFinal += array[i] + "\n";
+						}
+					}
+					JOptionPane.showMessageDialog(null, mensajeResultado, "Gracias por participar", JOptionPane.PLAIN_MESSAGE);
+					JOptionPane.showMessageDialog(null, logFinal, "Log de partidas", JOptionPane.PLAIN_MESSAGE);
+					System.exit(0);
+
+				/*"3" - Se gestionan aquellos eventos en los que es necesario deseleccionar un botón o los casos en los 
+				        que se ha presionado un botón sin oponente y hay que desbloquear todos los botones
+				        
+					 	* "nombreBotonMemoria" recoge temporalmente el nombre del botón que se ha presionado
+					 	   después, en servidor se comprueba si el "legal" que ese botón puede ser presionado */
+				} else if (input.startsWith("3")) {
+					mensajeFinal = input.substring(1);
+					JOptionPane.showMessageDialog(null, mensajeFinal, "Fin del juego", JOptionPane.INFORMATION_MESSAGE);
+					if (nombreBotonMemoria != null) {
+						deseleccionBoton();
+					}
+					if (mensajeFinal.equals("No tienes oponente")) {
+						desbloquearBotones();
+					}
 				
-				if (respuesta != null && !respuesta.isEmpty()) {
-					window.frmTresEnRaya.setVisible(true);
-				} else {
-					JOptionPane.showMessageDialog(null, "Cerrando la aplicación", "Cerrando",
-							JOptionPane.INFORMATION_MESSAGE);
-					System.exit(0);
-				}
-
-				// 4. ENVIAMOS EL NOMBRE AL SERVIDOR
-
-				try {
-					if (socketDelCliente != null) {
-						dos = new DataOutputStream(socketDelCliente.getOutputStream());
-						dos.writeUTF(respuesta);
-						lb_usuario.setText(respuesta);
-						
-					}
+				/* "4" - Se recoge la posición del botón que ha presionado el oponente, se actualiza el botón del jugador con 
+				         el movimiento del oponente y se  desbloquea su tablero para que el jugador pueda proceder con su turno */
+				} else if (input.startsWith("4")) {
+					System.out.println(input);
+					mensajeFinal = input.substring(1);
+					actualizarBoton(mensajeFinal);
+					desbloquearBotones();
 					
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(null, "No se ha podido conectar con el servidor", "Error",
-							JOptionPane.ERROR_MESSAGE);
+				// "5" - Se ejecuta el método "jugadaPermitida()" 
+				} else if (input.startsWith("5")) {
+					jugadaPermitida();
+				
+				// "6" - Gestiona el evento en el que el oponente se rinde y se termina el juego
+				} else if (input.equals("6")) {
+					JOptionPane.showMessageDialog(null, "Tu oponente se ha rendido", "Fin de partida", JOptionPane.INFORMATION_MESSAGE);
+					System.exit(0);
+					
+				// "7" - Gestiona el evento en el que tiene lugar un empate y termina el juego
+				} else if (input.equals("7")) {
+					JOptionPane.showMessageDialog(null, "Se ha creado un empate!!", "Fin del juego", JOptionPane.INFORMATION_MESSAGE);
+					System.exit(0);
+					
+				// "8" - Simplemente de desbloquean los botones del tablero
+				} else if (input.equals("0")) {
+					desbloquearBotones();
 				}
-
-				try {
-					dis = new DataInputStream(socketDelCliente.getInputStream());
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				while (true) {
-					// 5. ESTÁ CONSTANTEMENTE ESCUCHANDO A LA INFORMACIÓN DE LOS JUGADORES
-					try {
-
-						String input = dis.readUTF();
-
-						if (input.startsWith("1")) {
-							// estamos recibiendo el simbolo para asignarselo al cliente
-							valorSimbolo = (input.charAt(1) + "");
-							if (valorSimbolo.equals("X")) {
-								valorSimboloOponente = "O";
-							} else {
-								valorSimboloOponente = "X";
-								bloquearBotones();
-							}
-							lb_simbolo.setText(valorSimbolo);
-						} else if (input.startsWith("2")) {
-							mensajeFinal = input.substring(1);
-							String mensajeResultado = mensajeFinal.split("\n")[0];
-							
-							String logFinal= "";
-							String[] array  = mensajeFinal.split("\n");
-							for (int i = 0; i < array.length; i++) {
-								if(i!=0) {
-									logFinal+=array[i]+"\n";
-								}
-							}									
-							JOptionPane.showMessageDialog(null, mensajeResultado, "Gracias por participar", JOptionPane.PLAIN_MESSAGE);
-							JOptionPane.showMessageDialog(null, logFinal, "Log de partidas", JOptionPane.PLAIN_MESSAGE);
-						    
-							System.exit(0);
-						} else if (input.startsWith("3")) {
-							mensajeFinal = input.substring(1);
-							
-							JOptionPane.showMessageDialog(null, mensajeFinal,"Fin del juego", JOptionPane.INFORMATION_MESSAGE);
-							// DESSELECCIONAMOS EL BOTÓN ERRONEO
-							if(nombreBotonMemoria!=null) {
-								deseleccionBoton();
-							}
-							
-							if(mensajeFinal.equals("No tienes oponente")) {
-								desbloquearBotones();
-							}
-							
-						} else if (input.startsWith("4")) {
-							System.out.println(input);
-							mensajeFinal = input.substring(1);
-							actualizarBoton(mensajeFinal);
-							desbloquearBotones();
-						}else if(input.startsWith("5")) {
-							jugadaPermitida();
-						} else if(input.equals("6")) {
-							JOptionPane.showMessageDialog(null, "Tu oponente se ha rendido", "Cobarde", JOptionPane.INFORMATION_MESSAGE);
-							System.exit(0);
-						} else if(input.equals("7")) {
-							JOptionPane.showMessageDialog(null, "Se ha creado un empate!!", "Fin del juego", JOptionPane.INFORMATION_MESSAGE);
-							System.exit(0);
-						}else if(input.equals("0")) {
-							desbloquearBotones();
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-						System.exit(0); // para que no se quede en bucle
-					}
-				}
+				
+			// Si no se puede registrar el movimiento correctamente, se informa de ello y se sale del programa
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "No se ha podido registrar el movimiento\nSaliendo del programa", "Error", JOptionPane.ERROR_MESSAGE);
+				System.exit(0); // para que no se quede en bucle
 			}
+		}
+	}
 
-			private static void jugadaPermitida() {
-				for (int i = 0; i < arraybotones.length; i++) {
-					if (arraybotones[i].getName().equals(nombreBotonMemoria)) {
-						arraybotones[i].setEnabled(false);
-						arraybotones[i].setText(valorSimbolo);
-					}
-				}
-				nombreBotonMemoria=null;
+	// Recorre el array de botones y actualiza el tablero con el valor del botón con nombre = nombreBotonMemoria
+	private static void jugadaPermitida() {
+		for (int i = 0; i < arraybotones.length; i++) {
+			if (arraybotones[i].getName().equals(nombreBotonMemoria)) {
+				arraybotones[i].setEnabled(false);
+				arraybotones[i].setText(valorSimbolo);
 			}
+		}
+		nombreBotonMemoria = null;
+	}
 
-			private static void deseleccionBoton() {
-				for (int i = 0; i < arraybotones.length; i++) {
-					if (arraybotones[i].getName().equals(nombreBotonMemoria)) {
-						arraybotones[i].setEnabled(true);
-						arraybotones[i].setText("");
-					}
-				}
-				nombreBotonMemoria=null;
+	// Recorre el array de botones y actualiza el tablero deseleccionando el botón con nombre = nombreBotonMemoria
+	private static void deseleccionBoton() {
+		for (int i = 0; i < arraybotones.length; i++) {
+			if (arraybotones[i].getName().equals(nombreBotonMemoria)) {
+				arraybotones[i].setEnabled(true);
+				arraybotones[i].setText("");
 			}
+		}
+		nombreBotonMemoria = null;
+	}
 
-			private static void actualizarBoton(String mensajeFinal) {
-				for (int i = 0; i < arraybotones.length; i++) {
-					if(i==Integer.parseInt(mensajeFinal)) {
-						arraybotones[i].setText(valorSimboloOponente);
-						arraybotones[i].setEnabled(false);
-					}
-				}		
+	// Recorre el array de botones y actualiza con el botón con el movimiento del oponente
+	private static void actualizarBoton(String mensajeFinal) {
+		for (int i = 0; i < arraybotones.length; i++) {
+			if (i == Integer.parseInt(mensajeFinal)) {
+				arraybotones[i].setText(valorSimboloOponente);
+				arraybotones[i].setEnabled(false);
 			}
-		
-	
+		}
+	}
 
-	/**
-	 * Create the application.
-	 */
+	// Se inicializa "Cliente_view()"
 	public Cliente_view() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	// Se inicializan los contenidos del frame
 	private void initialize() {
 
 		frmTresEnRaya = new JFrame();
@@ -238,25 +232,25 @@ public class Cliente_view {
 		frmTresEnRaya.setResizable(false);
 
 		// Textos
-
 		JLabel lb_turno = new JLabel("Jugador:");
 		lb_turno.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lb_turno.setBounds(59, 259, 70, 19);
 		frmTresEnRaya.getContentPane().add(lb_turno);
-
 		lb_usuario = new JLabel("Jugador");
 		lb_usuario.setForeground(new Color(255, 0, 128));
 		lb_usuario.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lb_usuario.setBounds(124, 260, 186, 17);
 		frmTresEnRaya.getContentPane().add(lb_usuario);
-
 		JLabel lbl_recordatorio = new JLabel("Símbolo:");
 		lbl_recordatorio.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lbl_recordatorio.setBounds(59, 286, 58, 11);
 		frmTresEnRaya.getContentPane().add(lbl_recordatorio);
 
-		// Botones (A-C: columnas, numeros: filas)
-
+		/* Botones (A-C: columnas, 1-3: filas) y sus "on click listeners"	
+		   Cuando se presiona un botón del tablero se mete el nombre del botón en la variable
+		   "nombreBotonMemoria" y se envía la información de la posición presionada [0-9]		   
+		   También se indica el nombre del botón y su posición dentro del arrayBotones*/	
+		 
 		btn_A1 = new JButton("");
 		btn_A1.setForeground(Color.BLACK);
 		btn_A1.setFont(new Font("Tahoma", Font.BOLD, 30));
@@ -388,7 +382,7 @@ public class Cliente_view {
 		btn_C3.setBounds(215, 176, 70, 70);
 		frmTresEnRaya.getContentPane().add(btn_C3);
 		arraybotones[8] = btn_C3;
-		
+
 		lb_simbolo = new JLabel("Simbolo");
 		lb_simbolo.setForeground(new Color(255, 0, 128));
 		lb_simbolo.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -396,31 +390,38 @@ public class Cliente_view {
 		frmTresEnRaya.getContentPane().add(lb_simbolo);
 	}
 
+	/* Este método envía la posición del botón al servidor y bloquea los botones del tablero del usuario.
+	   Se incluye un "return" para salir del método y no bloquear los botones en caso de que no se pudieran
+	   enviar los datos correctamente */	
 	public static void enviarInfo(int posicion) {
-
-		// SE ENVÍA LA POSICIÓN DENTRO DEL ARRAY
 		try {
 			dos.writeInt(posicion);
 		} catch (Exception e) {
 			System.out.println("No se ha enviado 'botones'");
 			return;
 		}
-		
-		 bloquearBotones();
-		 
+		bloquearBotones();
 	}
 
+	// Este método bloquea todos los botones
 	private static void bloquearBotones() {
-		btn_A1.setEnabled(false); btn_A2.setEnabled(false); btn_A3.setEnabled(false);
-		 btn_B1.setEnabled(false); btn_B2.setEnabled(false); btn_B3.setEnabled(false);
-		 btn_C1.setEnabled(false); btn_C2.setEnabled(false); btn_C3.setEnabled(false);
+		btn_A1.setEnabled(false);
+		btn_A2.setEnabled(false);
+		btn_A3.setEnabled(false);
+		btn_B1.setEnabled(false);
+		btn_B2.setEnabled(false);
+		btn_B3.setEnabled(false);
+		btn_C1.setEnabled(false);
+		btn_C2.setEnabled(false);
+		btn_C3.setEnabled(false);
 	}
-	
-	private static void desbloquearBotones() {		
+
+	// Este método desbloquea todos los botones que no contienen texto, y por tanto, no han sido pulsados
+	private static void desbloquearBotones() {
 		for (int i = 0; i < arraybotones.length; i++) {
-			if(arraybotones[i].getText()=="") {
+			if (arraybotones[i].getText() == "") {
 				arraybotones[i].setEnabled(true);
-			}			
+			}
 		}
 	}
 }
--- RamosReyesSáenzProyecto2Evaluación/src/ServidorTCP.java
+++ RamosReyesSáenzProyecto2Evaluación/src/ServidorTCP.java
@@ -30,36 +30,40 @@ import javax.crypto.SecretKey;
 
 public class ServidorTCP {
 
+	// Variables para la gestión de las conexiones con los clientes
 	public static DataInputStream disJugador1, disJugador2;
 	public static Socket socketDelCliente1, socketDelCliente2;
 
 	public static void main(String[] args) {
 
-		Integer turno = 0;
-		Boolean jugando = true, jugador;
-
-		// FASE INICIAL
-		// Primero indicamos la dirección IP local
+		// Se consigue e indica la dirección IP del dispositivo ejecutando la clase "ServidorTCP"
 		try {
 			System.out.println("LocalHost = " + InetAddress.getLocalHost().toString());
 		} catch (UnknownHostException uhe) {
 			System.err.println("No puedo saber la dirección IP local : " + uhe);
 		}
 
-		// Abrimos un "Socket de Servidor" TCP en el puerto 1234.
-
+		//Se intenta abrir un socket de servidor TCP en el puerto 40000, limitando el número de conexiones a dos		
 		try (ServerSocket socketDelServidor = new ServerSocket(40000, 2)) {
-			// Se crea el socket servidor y se limita las conexiones a dos
-
+			
 			System.out.println("Esperando a que se conecten los usuarios...");
-
-			System.out.println("Tic Tac Toe Server is Running...");
-			var pool = Executors.newFixedThreadPool(200);
+			System.out.println("[El servidor se encuentra activo]");
+			
+			// Se crea la variable "pool" que permite crear hasta 20 hilos de forma concurrente
+			var pool = Executors.newFixedThreadPool(20);
+			
+			// Se crea un objeto de la clase "Juego"
 			Juego juego = new Juego();
 
+			// Se crean los sockets de los clientes, sus DataInputStreams
 			socketDelCliente1 = socketDelServidor.accept();
 			disJugador1 = new DataInputStream(socketDelCliente1.getInputStream());
+			
+			// El servidor queda a la espera de recibir el nombre de usuario de sus jugadores
 			String nombreJugador1 = disJugador1.readUTF();
+			
+			// So todo ha ido bien, pool ejecutará un proceso en el que se une un jugador al juego 
+			// Se aumenta el número de jugadores a uno
 			pool.execute(juego.new Jugador(nombreJugador1, socketDelCliente1, 'X'));
 			juego.numeroJugadores++;
 
@@ -69,8 +73,8 @@ public class ServidorTCP {
 			pool.execute(juego.new Jugador(nombreJugador2, socketDelCliente2, 'O'));
 			juego.numeroJugadores++;
 
-		
-
+			/* A partir de ahora, en el caso de que el número de jugadores sea menor de dos, significará que uno de
+			   ellos ha salido o se ha perdido conexión, por lo que cerramos todos los sockets y la ejecución*/
 			while (true) {
 				if (juego.numeroJugadores != 2) {
 					socketDelCliente1.close();
@@ -79,26 +83,26 @@ public class ServidorTCP {
 					System.exit(0);
 				}
 			}
-
 		} catch (IOException ioe) {
 			System.err.println("Error al abrir el socket de servidor : " + ioe);
 			System.exit(-1);
 		}
-
 	}
 }
 
-// CLASE JUEGO
+// Clase "Juego"
 class Juego {
-	// Se inicializa el tablero
+	
+	// Variables para la gestión de la clase "Juego"
 	static Jugador[] tablero = new Jugador[9];
 	public Boolean jugando = true;
 	public int turno = 0;
 	static Jugador jugadorActivo;
-	public static int numeroJugadores;
+	public int numeroJugadores;
 	public SecretKey claveSecreta;
 	public Cipher cipher;
 
+	// Este método valora si se ha dado un evento de victoria en el tablero
 	public boolean seHaGanado(Jugador[] tablero) {
 		return (tablero[0] != null && tablero[0] == tablero[1] && tablero[0] == tablero[2])
 				|| (tablero[3] != null && tablero[3] == tablero[4] && tablero[3] == tablero[5])
@@ -110,74 +114,68 @@ class Juego {
 				|| (tablero[2] != null && tablero[2] == tablero[4] && tablero[2] == tablero[6]);
 	}
 
-	public void logDePartidas() {
-
-	}
-
+	// Este método evita que tengan lugar múltiples eventos en el juego
 	public synchronized void checkJugada(int posicion, Jugador jugador) {
 		if (jugador != jugadorActivo) {
 			throw new IllegalStateException("No es tu turno. ");
 		} else if (jugador.oponente == null) {
-
 			throw new IllegalStateException("No tienes oponente");
 		} else if (tablero[posicion] != null) {
 			throw new IllegalStateException("Celda ocupada");
 		}
 	}
 	
-	public void crearRegistroEncriptadoEnLog(String nombreGanador, String nombrePerdedor)
-			throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-			IllegalBlockSizeException, BadPaddingException {
+	// Este método crea un registro encriptado de los resultados de la partida y lo escribe en un documento "logDePartidas.txt"
+	public void crearRegistroEncriptadoEnLog(String nombreGanador, String nombrePerdedor) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter("logDePartidas.txt", true));
         LocalDateTime fechaHoraActual = LocalDateTime.now();
         
         String fechaFormateada = fechaHoraActual.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
-
 		String frase = "Partida con fecha " + fechaFormateada + " -> Ganador: " + nombreGanador + ", Perdedor: " + nombrePerdedor;
-
 		String textoEncriptado = Base64.getEncoder().encodeToString(frase.getBytes());
 
-		// Escribimos la frase en el fichero
 		writer.write(textoEncriptado + "\n");
 		writer.flush();
 		writer.close();
 	}
 
-	public String getRegistrosEncriptados() throws NoSuchAlgorithmException, IOException,
-			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+	// Este método desencripta el contenido de "logDePartidas.txt", lo almacena en una variable "resultado" y lo devuelve
+	public String getRegistrosDesencriptados() throws NoSuchAlgorithmException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 
 		BufferedReader reader = new BufferedReader(new FileReader("logDePartidas.txt"));
 		String frase, resultado = "";
 
-		while ((frase = reader.readLine()) != null) {
-
-			// Descifrar el texto
+		while ((frase = reader.readLine()) != null) {	
 			byte[] fraseDesencriptadaBytes = Base64.getDecoder().decode(frase.getBytes());
 			String faseDesencriptada = new String(fraseDesencriptadaBytes);
 			resultado+=faseDesencriptada+"\n";
 		}
 
+		reader.close();
 		return resultado;
 	}
 
 	
-	// CLASE JUGADOR
+	/* Clase "Jugador" (dentro de la clase "Juego") que implementa la interfaz "Runnable"
+	   Es necesario que la clase implemente "Runnable" para poder trabajar con ella dentro de hilos */	 	
 	class Jugador implements Runnable {
 
+		// Variables para la gestión de la clase "Jugador"
 		public DataInputStream dis;
 		public DataOutputStream dos;
-
 		String nombreJugador;
 		Jugador oponente;
 		Socket socketDelCliente;
 		char simbolo;
 
+		// Constructor 
 		public Jugador(String nombreJugador, Socket socketDelCliente, char simbolo) {
 			this.nombreJugador = nombreJugador;
 			this.socketDelCliente = socketDelCliente;
 			this.simbolo = simbolo;
 		}
 
+		// Getters y Setters
 		public String getNombreJugador() {
 			return nombreJugador;
 		}
@@ -210,14 +208,14 @@ class Juego {
 			this.oponente = oponente;
 		}
 
+		// Método de la interfaz "Runnable", se definen las tareas a realizar por los objetos de la clase Jugador
 		@Override
 		public void run() {
 			try {
 				dis = new DataInputStream(socketDelCliente.getInputStream());
 				dos = new DataOutputStream(socketDelCliente.getOutputStream());
 
-				// 1. Asignar el jugador activo y el oponente
-
+				// Asignar el jugador activo y el oponente dependiendo del símbolo asignado
 				if (this.simbolo == 'X') {
 					jugadorActivo = this;
 				} else {
@@ -229,43 +227,33 @@ class Juego {
 					}
 				}
 
+				/* Envío de la marca al cliente, los dos jugadores mandan su marca a su respectivo cliente	
+				   1 + DATO: estamos recibiendo el simbolo para asignarselo al cliente*/
+				dos.writeUTF("1" + this.simbolo);			
 
-				// 2. Envio de la marca al cliente, los dos jugadores mandan su marca a su
-				// resoectivo cliente
-
-				dos.writeUTF("1" + this.simbolo);
-				
-				// 1 + dato: estamos recibiendo el simbolo para asignarselo al cliente
-
-				// El jugador va a estar a la escucha de la posicion clickada por el cliente
-				// [enviarInfo()]
-
+				/* A partir de este momento el jugador va a estar a la escucha de la posicion clickada por el cliente -> enviarInfo()
+				   Mientras la variable "jugando" == true y el número de turnos sea menor que 9 (nº máximo de turnos posibles, nº de casillas) */								
 				while (jugando && turno<9) {
 					try {
 
-
-						int posicion = dis.readInt(); // AQUÍ DEBE ESPERAR A QUE EL CLIENTE PRESIONE UN BOTÓN
-
-						
-						// chequear lo jugada
+						// Aquí se espera a que el cliente presione un botón y comprobar la jugada
+						int posicion = dis.readInt(); 
 						checkJugada(posicion, this);
-
 						
-						// Actualizar el tablero
+						// Actualizar el tablero (el tablero es un array de objetos "Jugador")
 						tablero[posicion] = this;
 
-						// Se cambian los roles
+						// Se cambian los roles y se envía la posición al oponente
 						jugadorActivo = this.oponente;
-
-						// Se envía la posicion al oponente
 						oponente.dos.writeUTF("4" + posicion);
 						dos.writeUTF("5");
 
-						// Se manda el resultado al oponente
+						// Se comprueba si se ha ganado en el tablero. Si se ha ganado:
 						if (seHaGanado(tablero)) {
 
+							// Se crea un nuevo registro, se consiguen los resultados 
 							crearRegistroEncriptadoEnLog(this.nombreJugador, oponente.getNombreJugador());
-							String resultado = getRegistrosEncriptados();
+							String resultado = getRegistrosDesencriptados();
 							
 							// Enviar información al jugadorActivo
 							dos.writeUTF("2Has ganado, " + this.getNombreJugador()+"\n"+resultado);
@@ -296,7 +284,6 @@ class Juego {
 					} catch (IOException e) {
 						e.printStackTrace();
 					} catch (IllegalStateException e) {
-						System.out.println("Excepcion");
 						dos.writeUTF("3" + e.getMessage());
 						turno--;
 					} catch (InvalidKeyException e) {
